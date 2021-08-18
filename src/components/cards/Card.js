1688dffeee2989eb3768bbfa62ed16a8a20dc611
elcinp <elcinpars1@gmail.com>
2021-08-18 15:34:59 +0300

    tour-places

---
+++ src/components/cards/Card.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {data} from "../../helpers/Data"
+import "./Card.css"
+
+function Card() {
+
+
+    return (
+        <div className="card-container">
+
+            {data.map((card) => {
+                return (
+
+                    <div className="cards">
+                        <div className="title">
+                            <h2>{card.title}</h2>
+                        </div>
+
+                        <img src={card.image} alt={card.title} />
+                        <div className="card-over">
+                            <p>{card.desc}</p>
+                        </div>
+                    </div>
+                )
+            })}
+
+        </div>
+    )
+}
+
+export default Card;
+
+         
